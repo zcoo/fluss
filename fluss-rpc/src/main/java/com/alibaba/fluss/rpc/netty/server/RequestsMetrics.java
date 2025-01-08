a0a04d393a08a5400ace386533631a4246f7adfb
yuxia Luo <luoyuxia@alumni.sjtu.edu.cn>
2025-01-08 17:25:53 +0800

    [hotfix] Add prefix lookup metrics (#315)

--- fluss-rpc/src/main/java/com/alibaba/fluss/rpc/netty/server/RequestsMetrics.java
+++ fluss-rpc/src/main/java/com/alibaba/fluss/rpc/netty/server/RequestsMetrics.java
@@ -64,7 +64,11 @@ public class RequestsMetrics {
     public static RequestsMetrics createTabletServerRequestMetrics(MetricGroup serverMetricsGroup) {
         List<ApiKeys> apiKeys =
                 Arrays.asList(
-                        ApiKeys.PRODUCE_LOG, ApiKeys.PUT_KV, ApiKeys.LOOKUP, ApiKeys.FETCH_LOG);
+                        ApiKeys.PRODUCE_LOG,
+                        ApiKeys.PUT_KV,
+                        ApiKeys.LOOKUP,
+                        ApiKeys.FETCH_LOG,
+                        ApiKeys.PREFIX_LOOKUP);
         return new RequestsMetrics(serverMetricsGroup, apiKeys);
     }
 
