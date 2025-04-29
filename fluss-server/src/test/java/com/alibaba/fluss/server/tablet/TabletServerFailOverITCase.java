62f247e20c9a5c498c08f8d14d3773842fa51c9c
Benchao Li <libenchao.bruce@bytedance.com>
2025-04-29 21:07:08 +0800

    [server] Add IT for kill follower & leader servers (#807)

--- fluss-server/src/test/java/com/alibaba/fluss/server/tablet/TabletServerFailOverITCase.java
+++ fluss-server/src/test/java/com/alibaba/fluss/server/tablet/TabletServerFailOverITCase.java
@@ -23,14 +23,21 @@ import com.alibaba.fluss.metadata.TableBucket;
 import com.alibaba.fluss.metadata.TableDescriptor;
 import com.alibaba.fluss.metadata.TablePath;
 import com.alibaba.fluss.rpc.gateway.TabletServerGateway;
-import com.alibaba.fluss.rpc.messages.ApiMessage;
 import com.alibaba.fluss.server.log.LogSegment;
 import com.alibaba.fluss.server.testutils.FlussClusterExtension;
+import com.alibaba.fluss.server.zk.ZooKeeperClient;
+import com.alibaba.fluss.server.zk.data.LeaderAndIsr;
 import com.alibaba.fluss.types.DataTypes;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static com.alibaba.fluss.record.TestData.DATA1;
 import static com.alibaba.fluss.record.TestData.DATA_1_WITH_KEY_AND_VALUE;
@@ -39,6 +46,8 @@ import static com.alibaba.fluss.server.testutils.RpcMessageTestUtils.newProduceL
 import static com.alibaba.fluss.server.testutils.RpcMessageTestUtils.newPutKvRequest;
 import static com.alibaba.fluss.testutils.DataTestUtils.genKvRecordBatch;
 import static com.alibaba.fluss.testutils.DataTestUtils.genMemoryLogRecordsByObject;
+import static com.alibaba.fluss.testutils.common.CommonTestUtils.retry;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /** The ITCase for tablet server failover. */
@@ -103,16 +112,72 @@ class TabletServerFailOverITCase {
         FLUSS_CLUSTER_EXTENSION.startTabletServer(leader, true);
     }
 
-    private ApiMessage writeData(
+    @Test
+    void testKillServers() throws Exception {
+        FLUSS_CLUSTER_EXTENSION.assertHasTabletServerNumber(3);
+        TableDescriptor tableDescriptor =
+                TableDescriptor.builder()
+                        .schema(Schema.newBuilder().column("a", DataTypes.INT()).build())
+                        .distributedBy(1)
+                        .property(ConfigOptions.TABLE_REPLICATION_FACTOR, 2)
+                        .build();
+        TablePath tablePath = TablePath.of("test_failover", "test_kill_servers");
+        long tableId = createTable(FLUSS_CLUSTER_EXTENSION, tablePath, tableDescriptor);
+        TableBucket tb = new TableBucket(tableId, 0);
+
+        LeaderAndIsr leaderAndIsr = FLUSS_CLUSTER_EXTENSION.waitLeaderAndIsrReady(tb);
+        List<Integer> isr = new ArrayList<>(leaderAndIsr.isr());
+        int leader = leaderAndIsr.leader();
+        isr.remove(Integer.valueOf(leader));
+        int follower = isr.get(0);
+
+        // let's kil follower
+        FLUSS_CLUSTER_EXTENSION.stopTabletServer(follower);
+        ZooKeeperClient zkClient = FLUSS_CLUSTER_EXTENSION.getZooKeeperClient();
+
+        // the follower should be removed from isr
+        LeaderAndIsr expectedLeaderAndIsr1 =
+                leaderAndIsr.newLeaderAndIsr(leader, Collections.singletonList(leader));
+        retry(
+                Duration.ofMinutes(1),
+                () ->
+                        assertThat(zkClient.getLeaderAndIsr(tb).get())
+                                .isEqualTo(expectedLeaderAndIsr1));
+
+        // kill the leader again
+        FLUSS_CLUSTER_EXTENSION.stopTabletServer(leader);
+
+        // should be no leader
+        LeaderAndIsr expectedLeaderAndIsr2 =
+                expectedLeaderAndIsr1.newLeaderAndIsr(
+                        LeaderAndIsr.NO_LEADER, Collections.singletonList(leader));
+        retry(
+                Duration.ofMinutes(1),
+                () ->
+                        assertThat(zkClient.getLeaderAndIsr(tb).get())
+                                .isEqualTo(expectedLeaderAndIsr2));
+
+        // start the follower
+        // should still be no leader since the follower is out of isr, should be elected as leader
+        FLUSS_CLUSTER_EXTENSION.startTabletServer(follower);
+
+        // start the leader server, the leader should be the previous leader server
+        FLUSS_CLUSTER_EXTENSION.startTabletServer(leader);
+        retry(
+                Duration.ofMinutes(1),
+                () -> assertThat(zkClient.getLeaderAndIsr(tb).get().leader()).isEqualTo(leader));
+    }
+
+    private void writeData(
             TabletServerGateway tabletServerGateway, long tableId, boolean isLogTable)
             throws Exception {
         if (isLogTable) {
-            return tabletServerGateway
+            tabletServerGateway
                     .produceLog(
                             newProduceLogRequest(tableId, 0, 1, genMemoryLogRecordsByObject(DATA1)))
                     .get();
         } else {
-            return tabletServerGateway
+            tabletServerGateway
                     .putKv(
                             newPutKvRequest(
                                     tableId, 0, 1, genKvRecordBatch(DATA_1_WITH_KEY_AND_VALUE)))
