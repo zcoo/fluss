36684dccf073e8c3711b6e4d7a1115caa19a4eee
Jark Wu <jark@apache.org>
2025-05-14 09:36:29 +0800

    [test] Fix unstable test CommitLakeTableSnapshotITCase.testCommitDataLakeData (#867)

--- fluss-server/src/test/java/com/alibaba/fluss/server/replica/CommitLakeTableSnapshotITCase.java
+++ fluss-server/src/test/java/com/alibaba/fluss/server/replica/CommitLakeTableSnapshotITCase.java
@@ -83,6 +83,7 @@ class CommitLakeTableSnapshotITCase {
             int leaderServer = FLUSS_CLUSTER_EXTENSION.waitAndGetLeader(tb);
             TabletServerGateway leaderGateWay =
                     FLUSS_CLUSTER_EXTENSION.newTabletServerClientForNode(leaderServer);
+            FLUSS_CLUSTER_EXTENSION.waitUtilAllReplicaReady(tb);
 
             for (int i = 0; i < 10; i++) {
                 leaderGateWay
