d508ccac1c3c35cdcc29521de4b7ec7180249906
whenzhou.zc <whenzhou.zc@alibaba-inc.com>
2025-07-24 19:18:37 +0800

    [server] Coordinator Server Supports High-Available

--- fluss-server/src/test/java/org/apache/fluss/server/testutils/FlussClusterExtension.java
+++ fluss-server/src/test/java/org/apache/fluss/server/testutils/FlussClusterExtension.java
@@ -260,7 +260,7 @@ public final class FlussClusterExtension
             setRemoteDataDir(conf);
             coordinatorServer = new CoordinatorServer(conf);
             coordinatorServer.start();
-            waitUntilCoordinatorServerElected();
+            waitUntilCoordinatorLeaderElected();
             coordinatorServerInfo =
                     // TODO, Currently, we use 0 as coordinator server id.
                     new ServerInfo(
@@ -271,6 +271,7 @@ public final class FlussClusterExtension
         } else {
             // start the existing coordinator server
             coordinatorServer.start();
+            waitUntilCoordinatorLeaderElected();
             coordinatorServerInfo =
                     new ServerInfo(
                             0,
@@ -494,7 +495,7 @@ public final class FlussClusterExtension
                 () -> serverNode, rpcClient, TabletServerGateway.class);
     }
 
-    private void waitUntilCoordinatorServerElected() {
+    private void waitUntilCoordinatorLeaderElected() {
         waitUtil(
                 () -> {
                     return zooKeeperClient.getCoordinatorAddress().isPresent();
