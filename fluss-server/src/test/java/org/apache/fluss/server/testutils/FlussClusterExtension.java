db7a319eb02f2ec6da0f2d0d423fb65e091f32c9
whenzhou.zc <whenzhou.zc@alibaba-inc.com>
2025-11-14 16:13:15 +0800

    support coordinator epoch2

--- fluss-server/src/test/java/org/apache/fluss/server/testutils/FlussClusterExtension.java
+++ fluss-server/src/test/java/org/apache/fluss/server/testutils/FlussClusterExtension.java
@@ -271,6 +271,7 @@ public final class FlussClusterExtension
         } else {
             // start the existing coordinator server
             coordinatorServer.start();
+            waitUntilCoordinatorServerElected();
             coordinatorServerInfo =
                     new ServerInfo(
                             0,
